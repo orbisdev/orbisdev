753e773ff3697087beb24e19c9df944d9fee047b
Klairm <40839469+Klairm@users.noreply.github.com>
2020-07-24 15:41:43 +0200

    Update check-flex.sh

--- depends/check-flex.sh
+++ depends/check-flex.sh
@@ -2,4 +2,4 @@
 # check-flex.sh by Klairm, used check-wget.sh as tempalte from  Naomi Peori 
 
 ## Check for flex.
-flex -h 1> /dev/null || { echo "ERROR: Install wget before continuing."; exit 1; }
+flex -h 1> /dev/null || { echo "ERROR: Install flex before continuing."; exit 1; }
