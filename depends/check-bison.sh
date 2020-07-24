761ab34a008fa2260df45f5aa3dea7a79082ea74
Klairm <40839469+Klairm@users.noreply.github.com>
2020-07-24 15:41:35 +0200

    Delete wget word used from template

--- depends/check-bison.sh
+++ depends/check-bison.sh
@@ -2,4 +2,4 @@
 # check-bison.sh by Klairm, used check-wget.sh as tempalte from  Naomi Peori 
 
 ## Check for bison.
-bison -h 1> /dev/null || { echo "ERROR: Install wget before continuing."; exit 1; }
+bison -h 1> /dev/null || { echo "ERROR: Install bison before continuing."; exit 1; }
