5705e7971799086a8982519959b72272d2b08303
Klairm <40839469+Klairm@users.noreply.github.com>
2020-07-24 15:41:51 +0200

    Update check-m4.sh

--- depends/check-m4.sh
+++ depends/check-m4.sh
@@ -2,4 +2,4 @@
 # check-m4.sh by Klairm, used check-wget.sh as tempalte from  Naomi Peori 
 
 ## Check for m4.
-m4 --help 1> /dev/null || { echo "ERROR: Install wget before continuing."; exit 1; }
+m4 --help 1> /dev/null || { echo "ERROR: Install m4 before continuing."; exit 1; }
