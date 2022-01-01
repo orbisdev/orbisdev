e3370cc800fbe999d40389bd65b612f255fbc3b2
Antonio José Ramos Márquez <bigboss@siliconmini.local>
2022-01-01 14:20:33 +0100

    newyear test

--- scripts/003-liborbis.sh
+++ scripts/003-liborbis.sh
@@ -3,7 +3,7 @@
 
 ## Download the source code.
 if test ! -d "orbisdev-liborbis/.git"; then
-	git clone https://github.com/orbisdev/orbisdev-liborbis && cd orbisdev-liborbis || exit 1
+	git clone https://github.com/orbisdev/orbisdev-liborbis -b newyear && cd orbisdev-liborbis || exit 1
 else
 	cd orbisdev-liborbis &&
 		git pull && git fetch origin &&
@@ -14,6 +14,7 @@ fi
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Build and install.
+make --quiet -j $PROC_NR sources || { exit 1; }
 make --quiet -j $PROC_NR clean || { exit 1; }
 make --quiet -j $PROC_NR || { exit 1; }
 make --quiet -j $PROC_NR install || { exit 1; }
