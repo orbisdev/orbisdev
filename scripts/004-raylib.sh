9d4322a3805fe88bb0e6aaee9a5df5cdf89081bc
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2022-01-02 16:05:25 +0100

    Update raylib

--- scripts/004-raylib.sh
+++ scripts/004-raylib.sh
@@ -11,8 +11,6 @@ else
 fi
 
 ## Determine the maximum number of processes that Make can work with.
-PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Build and install.
 ./raylib4Orbis.sh
-make --quiet -j $PROC_NR install || { exit 1; }
